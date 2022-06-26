daf2b5457b7d149ad9d360e17402dfbb941a1bd8
Peter Booth <107762744+PeterJBooth@users.noreply.github.com>
2022-06-26 11:42:42 +0100

    Update ideas.py

--- ideas.py
+++ ideas.py
@@ -7,14 +7,4 @@
 # Adjustable line properties (i.e. name, colour, soils properties)
 # Pivot point
 # Sliding stuff
-
-
 # Cool Graphics
-
-a = [[1,2,3], [4,5,6]]
-b = [1,2]
-
-if b == a[0][0:2]:
-    print('y')
-print(a[0][0:2])
-
